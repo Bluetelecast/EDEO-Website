b8c6459ed7e84234a46af4984e9de362103e1355
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-02-04 11:40:36 +0100

    removed the unused component

--- src/Components/Donate.jsx
+++ src/Components/Donate.jsx
@@ -15,10 +15,10 @@ const Donate = ()=>{
     </div>
     <section className="">
     <h2 className='p-4 pb-0 text-start text-3xl sm:text-5xl mt-[5rem] text-blue-500'>Equity and Education for Everyone.</h2>
-    <p className="p-8 text-md text-start">
+    <p className="p-8 text-md text-start ">
     We are working for Afghan children and women, who need access to equal education and facilities, your donation of $ 50 - $ 100 directly goes to fund their bright future, please help us give a smile gifted to Afghan children and women.
     </p>
-    <p className='p-8 text-md text-start'>We at EDEO all team thank you, for your kindness and big heart of gifting smiles to others.</p>
+    <p className='p-8 text-md text-start text-blue-950'>We at EDEO all team thank you, for your kindness and big heart of gifting smiles to others.</p>
   </section>
   <div className='flex justify-center'>
   <button onClick={()=>navevegate('/what_we_do')} className='m-4 p-4 bg-blue-500  self-end text-white cursor-pointer'>Donate</button>
--- src/Components/Team.jsx
+++
@@ -1,9 +0,0 @@
-const Team = ()=>{
-    return (
-        <section>
-            <h1>Our Team</h1>
-        </section>
-    );
-};
-
-export default Team;
